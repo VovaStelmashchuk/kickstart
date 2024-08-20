6531dbec0261d1495e7d77e16f0241c5bbd0e7dd
Vova Stelmashchuk <vovochkastelmashchuk@gmail.com>
2024-08-20 16:27:33 +0000

    add some code

--- src/routers/home.js
+++ src/routers/home.js
@@ -12,11 +12,28 @@ async function homeHandler(request, h) {
   return h.view('home', { cards: uiCards }, { layout: 'layout' });
 }
 
+async function login(request, h) {
+  const { username, password } = request.payload;
+  console.log('login information start');
+  console.log(username, password);
+  console.log('login information end');
+  return h.view('login', {}, { layout: 'layout' });
+}
+
 export function home(server) {
   server.route({
     method: 'GET',
     path: '/',
     handler: homeHandler,
+    options: {
+      auth: false
+    }
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/login',
+    handler: login,
     options: {
       auth: false
     }
